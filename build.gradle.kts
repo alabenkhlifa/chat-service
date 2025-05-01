0e97678602b5c4c1079856c5300670ec3aacf5d3
alabenkhlifa <ala.khliifa@gmail.com>
2025-05-01 22:43:48 +0100

    Added configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,10 @@ extra["springCloudVersion"] = "2024.0.1"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+
+    // TODO: Uncomment the line below to add the discovery client library
+    // implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
